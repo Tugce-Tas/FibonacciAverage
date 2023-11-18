b6a20e2297ab98bcdce4487f7d1b1c554db7ac97
TuğçeTaş <tugcetastas.3@gmail.com>
2023-11-18 17:13:23 +0300

    first commit

---
+++ Fibonacci/FibonacciCalculator.cs
@@ -0,0 +1,26 @@
+﻿namespace Fibonacci;
+
+public class FibonacciCalculator
+{
+    public void Example()
+    {
+        int n, total = 0, a = 0, b = 1, count = 0;
+        double sum = 0, average;
+      
+        Console.Write("Sayı giriniz: ");
+        n = Convert.ToInt16(Console.ReadLine());
+
+        for (int i = 1; i<n; i++)
+        {   
+            total = a+b;
+            a = b;
+            b = total;
+            sum += total;
+            count++;
+            
+        }
+        
+        average = (sum + 1) / (count + 1);
+        Console.WriteLine(average);
+    }}
+    
